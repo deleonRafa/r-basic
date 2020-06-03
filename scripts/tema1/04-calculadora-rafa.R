b764f238606691964c1e9c69545b6e0c4f61f097
deleonRafa <rdleon@gmail.com>
2020-06-03 00:43:35 -0500

    Notas de Clases

---
+++ scripts/tema1/04-calculadora-rafa.R
@@ -0,0 +1,121 @@
+2*(3+5/2)
+
+2*((3+5)/2)
+
+2/3+5
+
+2%/%3+5
+
+2%%3+5
+
+2^3*5
+
+2^-4
+
+725%/%7
+
+725%%7
+
+#D = d * q + r
+#r = D - d * q
+#q = D%/%d
+#x = D%%d
+
+725 - 103*7
+
+pi
+
+2*pi
+
+3^pi
+
+pi^2
+
+Inf
+-Inf
+
+NA
+
+NaN
+
+5/0
+
+0/0
+
+2^50 #2^50 =  1.1259e+15 = 1125900000000000
+
+2^(-15) #2^(-15) = 3.051758e-05 = 0.00003051758
+
+c(2^30, 2^(-15),1, 2, 3/2)
+
+sqrt(25)
+
+exp(1)
+
+log(pi)
+
+abs(-pi)
+
+factorial(5) #5*4*3*2*1
+factorial(-5)
+factorial(0)
+
+choose(5,3)
+choose(3,5)
+
+log(4^6,4)
+6^log(4,6)
+
+#funciones trigonométricas
+sin(pi/2)
+cos(60) #el coseno de 60 grados nos da un valor negativo. Está mal.
+cos(60*pi/180) #pasamos los grados a rapianes y ya se calcula bien
+
+sin(60*pi/180) #= 0.8660254
+asin(0.8660254) # arcsin en radianes no nos da los 60 grados
+asin(0.8660254) *180/pi #ahora si nos da los 60 grados 
+
+asin(5) # arc sin x in [-1,1]
+
+acos(-8)
+
+sinpi(1/2) #es equivalente al sin(pin/2)
+tan(pi) # -1.224647e-16 ~ 0
+tan(pi/2) #1.633124e+16 ~ Inf
+
+
+print(sqrt(2),10)
+round(sqrt(2),3)
+floor(sqrt(2))
+ceiling(sqrt(2))
+trunc(sqrt(2))
+
+sqrt(2)^2-2 # = 4.440892e-16 es un claro ejemplo de comas flotantes 
+round(sqrt(2),4) ^2
+2^50 #= 1.1259e+15
+print(2^50,15) # = 1125899906842624 muestra el número sin notación cientifica
+print(2^50,2) #= 1.1e+15 muestra solo 2 digitos y luego la notación
+
+print(pi, 22) # 3.141592653589793115998 después de 16 dígitos, la precisión de R varía. este no es el valor de Pi
+
+round(1.15,1)
+round(1.25,1)
+round(1.35,1)
+round(1.45,1)
+round(1.55,1)
+
+round(sqrt(2),0)
+round(sqrt(2))
+
+round(digits = 5, sqrt(2)) #se invierten los parámetros pero mencionandolo
+round(sqrt(2), 5)
+round(5,sqrt(2))
+
+#cuidado con los números negativos
+floor(-3.45)
+ceiling(-3.45)
+trunc(-3.45)
+
+
+x=(pi^2)/2
+x
